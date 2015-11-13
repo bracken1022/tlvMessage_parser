dbb91feb81fed5383c35b6f4a807084c61bf5d3d
bracken1983 <bracken@I.local>
2015-11-14 00:09:09 +0800

    refacotr for tlv parser

--- tlv_parser.py
+++ tlv_parser.py
@@ -1,6 +1,11 @@
 
 #this is used for tlv grammer
 
+TLV_HEAD_LEN = 4
+SMALLEST_FOUR_BYTES = 4
+SMALLEST_EIGHT_BYTES = 8
+
+
 type_dict = {'01D0': 'USECONFIG', '000C': 'EPGROUP'}
 
 test_data = "01 D0 00 2C 00 0C 00 04 00 00 03 04 00 0B 00 04 01 02 03 04 00 0A 00 08 01 02 03 04 05 06 07 08 00 23 00 04 09 08 07 06"
@@ -9,13 +14,12 @@ test_data = "01 D0 00 2C 00 0C 00 04 00 00 03 04 00 0B 00 04 01 02 03 04 00 0A 0
 
 
 def IsSmallestLength(input):
-    SMALLEST_FOUR_BYTES = 4 + 4
-    SMALLEST_EIGHT_BYTES = 8 + 4
+    FOUR_BYTES_TLV = SMALLEST_FOUR_BYTES + TLV_HEAD_LEN
+    EIGHT_BYTES_TLV = SMALLEST_EIGHT_BYTES + TLV_HEAD_LEN
 
     return SMALLEST_FOUR_BYTES == len(input) or  SMALLEST_EIGHT_BYTES == len(input)
 
 def OutPutTheTlvBuffer(tlv_array):
-    TLV_HEAD_LEN = 4
 
     if tlv_array[0] + tlv_array[1] in type_dict:
         type_tlv = type_dict[tlv_array[0] + tlv_array[1]]
@@ -27,9 +31,6 @@ def OutPutTheTlvBuffer(tlv_array):
     print "type is %s\r\nlength is %s\r\nvalue is %s\r\n" % (type_tlv, length_tlv, value_tlv)
 
 def GetTheStartIndex(tlv_array):
-    TLV_HEAD_LEN = 4
-    SMALLEST_FOUR_BYTES = 4
-    SMALLEST_EIGHT_BYTES = 8
 
     length_tlv = int(tlv_array[2] + tlv_array[3], 16)
 
